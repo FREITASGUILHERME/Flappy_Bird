72ea331ca9568c4624be94416b15e9de29a2842f
Guilherme Freitas <gfreitasrodrigues7@gmail.com>
2022-10-14 23:13:41 -0300

    4 - adicionados ao projeto 1º Loop, 1º Const e bkg

--- jogo.js
+++ jogo.js
@@ -5,3 +5,33 @@ sprites.src = './sprites.png';
 
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
+
+const flappyBird = {
+    spriteX: 0,
+    spriteY: 0,
+    largura: 33,
+    altura: 24,
+    x: 10,
+    y: 50,
+
+    desenha() {
+
+        contexto.drawImage(
+            sprites,
+            flappyBird.spriteX, flappyBird.spriteY, //Sprite X, Sprite Y
+            flappyBird.largura, flappyBird.altura, //Tamanho do recorte na Sprite
+            flappyBird.x, flappyBird.y, //Posição dentro do canvas
+            flappyBird.largura, flappyBird.altura //Tamanho dentro do canvas
+        );
+    }
+}
+
+function loop() {
+
+    flappyBird.desenha();
+
+    requestAnimationFrame(loop);
+
+}
+
+loop();
