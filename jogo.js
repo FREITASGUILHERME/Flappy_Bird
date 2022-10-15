b2f76329353c86ca5e20e13e22403c45c1df8202
Guilherme Freitas <gfreitasrodrigues7@gmail.com>
2022-10-14 23:46:19 -0300

    5 - Escopo completo

--- jogo.js
+++ jogo.js
@@ -6,6 +6,8 @@ sprites.src = './sprites.png';
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
 
+
+// [Pássaro]
 const flappyBird = {
     spriteX: 0,
     spriteY: 0,
@@ -26,9 +28,73 @@ const flappyBird = {
     }
 }
 
+
+//[Chão]
+const chao = {
+    spriteX: 0,
+    spriteY: 610,
+    largura: 224,
+    altura: 122,
+    x: 0,
+    y: canvas.height - 111,
+
+    desenha() {
+
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY, //Sprite X, Sprite Y
+            chao.largura, chao.altura, //Tamanho do recorte na Sprite
+            chao.x, chao.y, //Posição dentro do canvas
+            chao.largura, chao.altura //Tamanho dentro do canvas
+        );
+
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY, //Sprite X, Sprite Y
+            chao.largura, chao.altura, //Tamanho do recorte na Sprite
+            (chao.x + chao.largura), chao.y, //Posição dentro do canvas
+            chao.largura, chao.altura //Tamanho dentro do canvas
+        );
+    }
+}
+
+// [Plano de Fundo]
+const planodefundo = {
+    spriteX: 390,
+    spriteY: 0,
+    largura: 275,
+    altura: 104,
+    x: 0,
+    y: canvas.height - 215,
+
+    desenha() {
+
+        contexto.drawImage(
+            sprites,
+            planodefundo.spriteX, planodefundo.spriteY, //Sprite X, Sprite Y
+            planodefundo.largura, planodefundo.altura, //Tamanho do recorte na Sprite
+            planodefundo.x, planodefundo.y, //Posição dentro do canvas
+            planodefundo.largura, planodefundo.altura //Tamanho dentro do canvas
+        );
+
+
+        contexto.drawImage(
+            sprites,
+            planodefundo.spriteX, planodefundo.spriteY, //Sprite X, Sprite Y
+            planodefundo.largura, planodefundo.altura, //Tamanho do recorte na Sprite
+            (planodefundo.x + planodefundo.largura), planodefundo.y, //Posição dentro do canvas
+            planodefundo.largura, planodefundo.altura //Tamanho dentro do canvas
+        );
+    }
+}
+
 function loop() {
 
     flappyBird.desenha();
+
+    chao.desenha();
+
+    planodefundo.desenha();
 
     requestAnimationFrame(loop);
 
